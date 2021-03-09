f1e427e5391d0a3bdcfae904b782176edc41c027
franciscowendel <iamfranciscowendel@gmail.com>
2021-03-09 18:36:23 -0300

    Implementando o arquivo 'userverify.py'

--- store/userverify.py
+++ store/userverify.py
@@ -1,6 +1,7 @@
-from passlib.hash import pbkdf2_sha256 as cryp
+from passlib.hash import pbkdf2_sha256 as key
 
 
 class User:
     def __init__(self, nome: str, email: str, senha: str) -> None:
-        pass
+        self.__nome: str = nome
+
